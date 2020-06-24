a65c97e92c5f8871fdf365507921503ff5ac9d09
Niyaaz Moosa <nmoosa@c4r7s8.wethinkcode.co.za>
2020-06-24 10:47:56 +0200

    done

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -6,34 +6,14 @@
 /*   By: nmoosa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 11:13:10 by nmoosa            #+#    #+#             */
-/*   Updated: 2020/06/21 13:36:58 by nmoosa           ###   ########.fr       */
+/*   Updated: 2020/06/24 10:47:34 by nmoosa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ultimate_ft(int *********nbr);
 
-void	ft_ultimate_ft(int *********nbr){
-		
-		int	a;
-		int	*nbr1;
-		int	**nbr2;
-		int	***nbr3;
-		int	****nbr4;
-		int	*****nbr5;
-		int	******nbr6;
-		int	*******nbr7;
-		int	********nbr8;
-
-		a = 42;
-		nbr1 = &a;
-		nbr2 = &nbr1;
-		nbr3 = &nbr2;
-		nbr4 = &nbr3;
-		nbr5 = &nbr4;
-		nbr6 = &nbr5;
-		nbr7 = &nbr6;
-		nbr8 = &nbr7;
-		nbr = &nbr8;
+void	ft_ultimate_ft(int *********nbr)
+{
+		*********nbr = 42;		
 }
 
 
